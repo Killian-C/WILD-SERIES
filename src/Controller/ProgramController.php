01088c56bc12a41c85574d487dec01a6d4d2bd69
Killian <killian.couraillon@hotmail.fr>
2021-05-18 18:59:05 +0200

    Configuration of programs + id routes

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,15 +6,28 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route ("/programs", name="program_")
+ */
 class ProgramController extends AbstractController
 {
     /**
-     * @Route ("/programs/", name="program_index")
+     * @Route ("/", name="program_index")
      */
     public function index(): Response
     {
         return $this->render('program/index.html.twig', [
             'website' => 'Wild Series',
+        ]);
+    }
+
+    /**
+     * @Route ("/{page}", requirements={"page"="\d+"}, methods={"GET"}, name="program_show")
+     */
+    public function show(int $page): Response
+    {
+        return $this->render('program/show.html.twig', [
+            'id' => $page
         ]);
     }
 }
