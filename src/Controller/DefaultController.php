7ae8cc3755ce41bd42e30f4716edfd7183c51120
Killian <killian.couraillon@hotmail.fr>
2021-07-02 19:08:40 +0200

    Finish Quest26

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 
 namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,4 +16,5 @@ class DefaultController extends AbstractController
     {
         return $this->render('index.html.twig');
     }
+
 }
